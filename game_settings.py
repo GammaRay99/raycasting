3ac2b1abc3d0918e8f00605bb3a990c84f46e81c
GammaRay99 <52253862+GammaRay99@users.noreply.github.com>
2021-02-10 22:22:36 +0100

    Update game_settings.py

--- game_settings.py
+++ game_settings.py
@@ -5,35 +5,38 @@ Contains:
     -game constants
     -ray constants
 """
-
+import pygame
 import math
 
 
 # global window constants
 WIDTH = 1200
 HEIGHT = 800
-FPS = 120
+FPS = 60
 
 
 # ray cast constants
 FOV = math.pi / 3  # we're working with trigonometric values
-NUM_RAYS = 60
+NUM_RAYS = 80
 MAX_LENGTH = 800  # The maximal length of the rays
 DELTA_ANGLE = FOV / NUM_RAYS  # This is the angle between rays
 RAY_WIDTH = WIDTH // NUM_RAYS
 
 
 # game constants
+BUTTON_SIZE = (500, 400)
 TILE = 100
 BG_COLOR = 200 / (1 + MAX_LENGTH * MAX_LENGTH * 0.00001)  # this represent the color of the sky, but also the color
                                                           # of the rays that are far away, it creates a fog effect.
+FONT = lambda size: pygame.font.SysFont('verdana', size, True)
 COLORS = {
-    'white':  (255, 255, 255),
-    'black':  (0, 0, 0),
+    'white':  (200, 200, 200),
+    'black':  (50, 50, 50),
     'orange': (150, 80, 40),
     'green':  (0, 200, 0),
     'blue':   (0, 0, 200),
     'gray':   (150, 150, 150),
     'purple': (120, 0, 120),
+    'red':    (200, 0, 0),
     'bg':     (BG_COLOR, BG_COLOR, BG_COLOR)
 }
