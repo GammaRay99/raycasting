f36a19e131875e477300993edd8a74617fa36aef
GammaRay99 <52253862+GammaRay99@users.noreply.github.com>
2021-02-10 10:55:25 +0100

    Update map_world.py

--- map_world.py
+++ map_world.py
@@ -1,14 +1,23 @@
+"""
+This module handles the map gestion of the scene.
+Contains:
+    -the map
+    -all the walls-objects
+    -all the walls positions
+"""
+
+# LOCAL IMPORTS
 import classes
 import game_settings
 
 map_array = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
@@ -17,6 +26,9 @@ walls_cord = []
 
 for y, row in enumerate(map_array):
     for x, tile in enumerate(row):
+        """
+        We're iterating in the matrix and extracting x&y of each wall
+        """
         if tile:
             walls_cord.append((x, y))
-            walls.append(classes.Wall(x * game_settings.TILE, y * game_settings.TILE))
+            walls.append(classes.Wall((x * game_settings.TILE, y * game_settings.TILE)))
